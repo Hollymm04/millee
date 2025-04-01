866db36b49b794e7fdb95ff5709fe5418dba786d
Hollymm04 <Hollymm04@vt.edu>
2025-04-01 01:52:39 -0400

    npm vercel stuff

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
+const app = express();
 
-app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    console.log('Here')
-    
-    res.render('index')
+    res.render('index');
 
-})
-
-app.listen(3000)
+});
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log('Server running on http://localhost:${PORT}');
+});

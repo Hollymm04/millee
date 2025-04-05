3c9ce4cfd29fde7efa29c8a37cc1c665f491cd6e
Hollymm04 <Hollymm04@vt.edu>
2025-04-05 17:46:43 -0400

    yuh

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'views')));
 
-app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('index');

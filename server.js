2dd2025852527ad1ab9d48650d93e25b32fb5fb6
Hollymm04 <Hollymm04@vt.edu>
2025-04-05 17:39:25 -0400

    .....

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('index');
+    res.render('index');
 });
 
 module.exports = app;
